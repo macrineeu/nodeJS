394b64b5835ea2be4fb8603deabe255d80e6a75e
Matheus Macrineu <macrineu.developer@gmail.com>
2022-12-09 19:26:20 -0300

    Manipulação de arquivos

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const fs = require('fs');
 const hostname = "127.0.0.1";
 const port = "8080";
 
+//Criando Novo Arquivo
+fs.writeFile('test.txt', 'Testando a criação de arquivo', (err) => {
+    if (err)
+        throw err;
+
+    console.log('O arquivo foi criado com sucesso!')
+})
+
+//Cria arquivo ou Insere conteudo no Final
+fs.appendFile('test.txt', '\n Outro conteudo seila', (err) => {
+    if (err)
+        throw err;
+
+    console.log('O arquivo foi salvo com sucesso!')
+})
+
 const server = http.createServer((req, res) => {
     fs.readFile('index.html', (err, data) => {
         res.writeHead(200, {'Content-Type': 'text/html'});
