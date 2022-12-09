ed3c97d4b0654d5b50f49a55a76d5b879ae0c1d5
Matheus Macrineu <macrineu.developer@gmail.com>
2022-12-09 19:18:25 -0300

    retornando HTML para o usuario acessar o servidor NODE

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = "127.0.0.1";
 const port = "8080";
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-
-    res.end("Hello World!");
+    fs.readFile('index.html', (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
 })
 
 server.listen(port, hostname, () => {
